e83f6e54a0dd5527fe259f0a4ea25aea55110994
Sajana21 <ST10433940@vcconnect.edu.za>
2025-10-21 11:02:15 +0200

    Added Claim Model

---
+++ PROG_CMCS_Part1/Models/Claim.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PROG_CMCS_Part1.Models
+{
+    public class Claim
+    {
+        public int Id { get; set; }
+        public string LecturerName { get; set; }
+        public string ModuleCode { get; set; }
+        public int HoursWorked { get; set; }
+        public decimal HourlyRate { get; set; }
+        public decimal TotalAmount => HoursWorked * HourlyRate;
+        public string? Comments { get; set; }
+        public string Status { get; set; }
+        public string CoordinatorName { get; set; } = "-";
+        public string CoordinatorStatus { get; set; } = "Pending";
+        public string ManagerName { get; set; } = "-";
+        public string ManagerStatus { get; set; } = "Pending";
+        public string Month { get; set; }
+        public DateTime DateSubmitted { get; set; }
+
+        public List<string> EncryptedDocuments { get; set; } = new List<string>();
+        public List<string> OriginalDocuments { get; set; } = new List<string>();
+    }
+}
