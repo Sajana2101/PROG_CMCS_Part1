1eca878eb45e020b7153dfd6d4eb22316450ded5
Sajana21 <ST10433940@vcconnect.edu.za>
2025-10-21 13:40:28 +0200

    Updated the Coordinator Dashboard so that the table headings are in black

--- PROG_CMCS_Part1/Models/Claim.cs
+++ PROG_CMCS_Part1/Models/Claim.cs
@@ -6,18 +6,18 @@ namespace PROG_CMCS_Part1.Models
     public class Claim
     {
         public int Id { get; set; }
-        public string LecturerName { get; set; }
-        public string ModuleCode { get; set; }
+        public string? LecturerName { get; set; }
+        public string ?ModuleCode { get; set; }
         public int HoursWorked { get; set; }
         public decimal HourlyRate { get; set; }
         public decimal TotalAmount => HoursWorked * HourlyRate;
         public string? Comments { get; set; }
-        public string Status { get; set; }
+        public string? Status { get; set; }
         public string CoordinatorName { get; set; } = "-";
         
         public string ManagerName { get; set; } = "-";
       
-        public string Month { get; set; }
+        public string? Month { get; set; }
         public DateTime DateSubmitted { get; set; }
 
         public List<string> EncryptedDocuments { get; set; } = new List<string>();
